9f0ef2cfdc430e213051537683c840411cc969ff
“zojian” <hellozojian@gmail.com>
2018-05-13 12:55:06 +0800

    fix:第一次获取朋友圈

--- utils/feed_utils.py
+++ utils/feed_utils.py
@@ -1,6 +1,7 @@
 from utils import redis_utils
 import time
 
+
 def send(user_id, image_id, date):
     """
     给好友的feed集合条件该图片信息
@@ -8,23 +9,24 @@ def send(user_id, image_id, date):
     :param image_id:
     :return:
     """
+    print(user_id, image_id, date)
     # 所有粉丝列表
     score = time.mktime(date.timetuple())
     conn = redis_utils.get_connection()
-    pipe = conn.pipeline()
-    try:
-        # 添加进自己的朋友圈
-        key = 'moments:' + str(user_id)
-        pipe.zadd(key, image_id, score)
-        key = 'user:' + str(user_id) + ':followings'
-        followings_list = pipe.zrange(key, 0, -1)
-        # 遍历好友 推图片
-        for id in followings_list:
-            key = 'moments:'+ bytes.decode(id)
-            pipe.zadd(key, image_id, score)
-        pipe.execute()
-    except Exception:
-        print('send error')
+    # 添加进自己的朋友圈
+    key = 'moments:' + str(user_id)
+    conn.zadd(key, image_id, score)
+    print('>>>>>>>my-moments:add success...')
+    key = 'user:' + str(user_id) + ':followings'
+    print('>>>>key:',key)
+    followings_list = conn.zrange(key, 0, -1)
+    print('>>>>>list:',followings_list)
+    # 遍历好友 推图片
+    for id in followings_list:
+        key = 'moments:'+ bytes.decode(id)
+        print(">>>>>key:",key)
+        conn.zadd(key, image_id, score)
+        print(">>>>>friend-moments:add success...")
 
 
 def follow_user(from_user_id, to_user_id):
@@ -70,8 +72,27 @@ def get_moments(user_id, page, len):
     key = 'moments:' + str(user_id)
     nums = conn.zcard(key)
     images_id = conn.zrevrange(key, page*len, (page+1)*len-1)
+    print('>>>>>images_id:',images_id)
+    if not images_id:
+        key = 'user:'+str(user_id)+':followings'
+        followings_list = conn.zrange(key, 0, -1)
+        print('>>>>followings_list',followings_list)
+        for id in followings_list:
+            key = 'user:'+bytes.decode(id) + ':images'
+            print(key)
+            images = conn.zrange(key,0,-1,withscores=True)
+            print(images)
+            key = 'moments:'+str(user_id)
+            for image_id,score in images:
+                conn.zadd(key, image_id, score)
+        key = 'moments:' + str(user_id)
+        nums = conn.zcard(key)
+        images_id = conn.zrevrange(key, page * len, (page + 1) * len - 1)
+        return {"moments_num": nums, "images_id": [int(bytes.decode(id)) for id in images_id]}
+    else:
+        return {"moments_num": nums, "images_id": [int(bytes.decode(id)) for id in images_id]}
     # result = []
     # for id in images_id:
     #     result.append(redis_utils.get_image_info(bytes.decode(id)))
     # return result
-    return {"moments_num":nums,"images_id":[int(bytes.decode(id)) for id in images_id]}
+    # return {"moments_num":nums,"images_id":[int(bytes.decode(id)) for id in images_id]}
